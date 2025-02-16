a6634593508e529cfbe539658f369eabfb4d0f4b
aiumar473 <149413180+aiumar473@users.noreply.github.com>
2025-02-16 07:55:52 +0000

    i learn complete about variable

---
+++ 01_basic_js/variable.js
@@ -0,0 +1,15 @@
+const accountId = 112
+let email="umar@gmail.com"
+var pass="12345"
+accountCity="karachi"
+
+//accountId=1223 not allow
+email="lai@gmail.com"
+pass="15"
+accountCity="hyderabad"
+console.log(accountId)
+console.table([accountId,accountCity,email,pass])
+
+/* 
+prefer not to use var because it issue on blog scope and funtional scope
+*/
